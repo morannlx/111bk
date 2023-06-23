72f67e587b8fadd9dbd84bd15d4fe60eaad931dc
墨染__nlx <78503566+morannlx@users.noreply.github.com>
2023-06-23 14:21:52 +0800

    Create more.js

---
+++ source/js/more.js
@@ -0,0 +1,16 @@
+// 浏览器搞笑标题
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function() {
+	if (document.hidden) {
+		$('[rel="icon"]').attr('href', "/funny.ico");
+		document.title = '╭(°A°`)╮ 不要丢弃我';
+		clearTimeout(titleTime);
+	} else {
+		$('[rel="icon"]').attr('href', "/img/newtubiao.png");
+		document.title = '(ฅ>ω<*ฅ) 感谢你能回来' + OriginTitle;
+		titleTime = setTimeout(function() {
+			document.title = OriginTitle;
+		}, 2000);
+	}
+});

b637642b3a10d506ae468fb6551ca06fa0c341b3
墨染__nlx <78503566+morannlx@users.noreply.github.com>
2023-07-02 12:29:38 +0800

    更新 more.js

--- source/js/more.js
+++ source/js/more.js
@@ -4,11 +4,11 @@ var titleTime;
 document.addEventListener('visibilitychange', function() {
 	if (document.hidden) {
 		$('[rel="icon"]').attr('href', "/funny.ico");
-		document.title = '╭(°A°`)╮ 不要丢弃我';
+		document.title = ':-( 页面已崩溃';
 		clearTimeout(titleTime);
 	} else {
 		$('[rel="icon"]').attr('href', "/img/newtubiao.png");
-		document.title = '(ฅ>ω<*ฅ) 感谢你能回来' + OriginTitle;
+		document.title = ':) 页面已恢复' + OriginTitle;
 		titleTime = setTimeout(function() {
 			document.title = OriginTitle;
 		}, 2000);
